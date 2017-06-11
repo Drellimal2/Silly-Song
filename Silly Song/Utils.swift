555fb8ab624ca5696d9cf83cc3bfa01399af1d7e
Dane Miller <dane@Danes-MacBook-Pro.local>
2017-06-11 01:22:41 -0500

    Added UI components

--- Silly Song/Utils.swift
+++ Silly Song/Utils.swift
@@ -8,15 +8,29 @@
 
 import Foundation
 
+let bananaFanaTemplate = [
+    "♪♪♪<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>♪♪♪"].joined(separator: "\n")
 
-func customizeTemplate(name: String, template : String) {
+
+func customizeTemplate(name: String, template : String) -> String {
     // TODO
-    print(shortNameFromName(name: name))
+    let shortName = shortNameFromName(name: name)
+    let song = template.replacingOccurrences(of: "<FULL_NAME>", with: name).replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+    return song
 }
+
+
 
 
 func shortNameFromName(name : String) -> String {
-    
-    return name.substring(from: name.index(after: name.startIndex))
+    let lowercaseName = name.substring(from: name.index(after :name.startIndex)).lowercased()
+//    let vowelSet = CharacterSet.init(charactersIn: "aeiou")
+//    return lowercaseName.substring(from: lowercaseName.index(of: "a"))
+    return lowercaseName
     
 }
+
+
--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var songTextView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func reset(_ sender: Any) {
+        nameTextField.text = ""
+        songTextView.text = ""
+    }
 
+    @IBAction func displayLyrics(_ sender: Any) {
+        if !(nameTextField.text?.isEmpty)!{
+            songTextView.text = customizeTemplate(name: nameTextField.text!, template: bananaFanaTemplate)
+        }
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
 }
 
